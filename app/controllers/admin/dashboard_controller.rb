bc4043e8132e3bef53e0f0146e8636f057596b67
Alexander George <ageorge010@vt.edu>
2017-05-08 19:23:30 -0700

    update any changes

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,11 @@ class Admin::DashboardController < ShopifyApp::AuthenticatedController
   # GET /admin
   # GET /admin.json
   def index
-    @products = ShopifyAPI::Product.find(:all, :params => {:limit => 10})
+  end
+
+  def show
+    #get the product by id
+    @product = ShopifyAPI::Product.find(params[:id])
+
   end
 end
