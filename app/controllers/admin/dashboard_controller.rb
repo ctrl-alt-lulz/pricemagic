27a5bd627513ed98e786863981f9fe3cdcb98cb8
Alexander George <ageorge010@vt.edu>
2017-05-22 19:02:59 -0700

    created product page, shows product title

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,7 +3,6 @@ class Admin::DashboardController < ShopifyApp::AuthenticatedController
   # GET /admin.json
   def index
     @products = ShopifyAPI::Product.find(:all, :params => {:limit => 10})
-
   end
 
   def show
