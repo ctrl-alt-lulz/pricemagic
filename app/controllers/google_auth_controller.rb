77792d139f77b55a20b6f399f329a6ed6b8c6526
Patrick Cushing <patrick.j.cushing@gmail.com>
2017-06-08 19:50:08 -0700

    Pseudo code to prep for saving access token

--- app/controllers/google_auth_controller.rb
+++ app/controllers/google_auth_controller.rb
@@ -6,12 +6,12 @@ class GoogleAuthController < ApplicationController
 
   def new
     client = ::Signet::OAuth2::Client.new({
-    client_id: ENV.fetch('GOOGLE_API_CLIENT_ID'),
-    client_secret: ENV.fetch('GOOGLE_API_CLIENT_SECRET'),
-    authorization_uri: 'https://accounts.google.com/o/oauth2/auth',
-    scope: ::Google::Apis::AnalyticsreportingV4::AUTH_ANALYTICS_READONLY,
-    redirect_uri: "https://533e004c.ngrok.io/oauth2callback",
-    token_credential_uri:  'https://www.googleapis.com/oauth2/v3/token',
+      client_id: ENV.fetch('GOOGLE_API_CLIENT_ID'),
+      client_secret: ENV.fetch('GOOGLE_API_CLIENT_SECRET'),
+      authorization_uri: 'https://accounts.google.com/o/oauth2/auth',
+      scope: ::Google::Apis::AnalyticsreportingV4::AUTH_ANALYTICS_READONLY,
+      redirect_uri: "https://533e004c.ngrok.io/oauth2callback",
+      token_credential_uri:  'https://www.googleapis.com/oauth2/v3/token',
     })
     redirect_to client.authorization_uri.to_s
   end
@@ -19,22 +19,21 @@ class GoogleAuthController < ApplicationController
 
   def callback
     client = Signet::OAuth2::Client.new({
-    client_id: ENV.fetch('GOOGLE_API_CLIENT_ID'),
-    client_secret: ENV.fetch('GOOGLE_API_CLIENT_SECRET'),
-    token_credential_uri: 'https://accounts.google.com/o/oauth2/token',
-    redirect_uri: "https://533e004c.ngrok.io/oauth2callback",
-    code: params[:code]
-  })
+      client_id: ENV.fetch('GOOGLE_API_CLIENT_ID'),
+      client_secret: ENV.fetch('GOOGLE_API_CLIENT_SECRET'),
+      token_credential_uri: 'https://accounts.google.com/o/oauth2/token',
+      redirect_uri: "https://533e004c.ngrok.io/oauth2callback", ## TODO put this in .env
+      code: params[:code]
+    })
     response = client.fetch_access_token!
     session[:access_token] = response['access_token']
-    redirect_to url_for(:action => :analytics)
-  end
 
-  def analytics
-    client = Signet::OAuth2::Client.new(access_token: session[:access_token])
-    client.expires_in = Time.now + 1_000_000
-    service = Google::Apis::AnalyticsreportingV4::AnalyticsService.new
-    service.authorization = client
-    @account_summaries = service.list_management_account_summaries
+    ## TODO store access_token on user
+    # current_user.access_token = response['access_token']
+    # current_user.save
+
+    ## TODO redirect back to index (or wherever they started)
+    ## redirect_to admin_products_path
+    redirect_to url_for(:action => :analytics)
   end
 end
--- app/workers/query_google_api_worker.rb
+++ app/workers/query_google_api_worker.rb
@@ -1,6 +1,12 @@
 class QueryGoogleApiWorker
   include Sidekiq::Worker
+
   def perform
-      puts "Hello world"
+    ## lookup the user to get the access_token
+    client = Signet::OAuth2::Client.new(access_token: session[:access_token])
+    client.expires_in = Time.now + 1_000_000 ## TODO research more here
+    service = Google::Apis::AnalyticsreportingV4::AnalyticsService.new
+    service.authorization = client
+    @account_summaries = service.list_management_account_summaries
   end
 end
