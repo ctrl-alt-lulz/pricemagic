868da3bd3012f7ca8c103b5952f525291eebe8c5
Alex <ageorge010@vt.edu>
2017-09-08 02:22:26 +0000

    Adds ongoing react set up

---
+++ app/javascript/packs/PriceTestForm.js
@@ -0,0 +1,85 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import PropTypes from 'prop-types'
+import {Page, Card, Select, Button, TextField, Stack, FormLayout,
+Thumbnail, ResourceList, Pagination, Layout, Checkbox} from '@shopify/polaris';
+
+export default class PriceTestForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      percent_increase: '',
+      percent_decrease: '',
+      product: this.props.product
+    };
+    this.handlePercentIncreaseChange = this.handlePercentIncreaseChange.bind(this)
+    this.handlePercentDecreaseChange = this.handlePercentDecreaseChange.bind(this)
+  }
+  handlePercentIncreaseChange(event) {
+    this.setState({percent_increase: event}) 
+  }
+  handlePercentDecreaseChange(event) {
+    this.setState({percent_decrease: event}) 
+  }
+    render () {
+        return (<Card>
+                  <FormLayout>
+                    <FormLayout.Group>
+                      <TextField 
+                        value={this.state.percent_increase}
+                        label="Percent Increase"
+                        placeholder="Enter %"
+                        onChange={this.handlePercentIncreaseChange}
+                      />
+                      <TextField 
+                        value={this.state.percent_decrease}
+                        label="Percent Decrease"
+                        placeholder="Enter %"
+                        onChange={this.handlePercentDecreaseChange}
+                      />
+                    </FormLayout.Group>
+                    <FormLayout.Group>
+                      <Select
+                        value= '1' //{this.state.collection}
+                        label="Price Points"
+                        options={ ['1', '2', '3', '4', '5'] }
+                        placeholder="Select"
+                        //onChange={this.handleCollectionChange}
+                      />
+                      <Select
+                        value= '0.99' //{this.state.collection}
+                        label="Ending Digits"
+                        options={ ['.99', '0.95', '0.50', '0.00'] }
+                        placeholder="Select"
+                        //onChange={this.handleCollectionChange}
+                      />
+                    </FormLayout.Group>
+                    <Button primary>Start Price Test</Button>
+                 </FormLayout>
+               </Card>);
+    }
+  createPriceTest() {
+    $.ajax( {
+      type: "POST",
+      url: '/price_tests',
+      data: { product_id: this.state.product.id, 
+              shopify_product_id: this.state.product.shopify_id, 
+              percent_increase: this.state.percent_increase, 
+              ending_digits: "0.99", 
+              price_points: 1 },
+      success: function(data) {
+        console.log('success')
+        //this.setState({ products: data });
+      }.bind(this),
+      error: function(data) {
+      }.bind(this)
+     });
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const node = document.getElementById('polaris-price-test-form')
+  const data = JSON.parse(node.getAttribute('data'))
+ReactDOM.render(<PriceTestForm {...data}/>, node)
+})
--- app/javascript/packs/polaris.js
+++ app/javascript/packs/polaris.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
-import {Page, Card, Select, Button, TextField, Stack,
-Thumbnail, ResourceList, Pagination, Layout} from '@shopify/polaris';
+import PriceTestForm from './PriceTestForm.js'
+import { Page, Card, Select, Button, TextField, Stack, FormLayout,
+        Thumbnail, ResourceList, Pagination, Layout, Checkbox, 
+        FooterHelp } from '@shopify/polaris';
 
 class ProductIndexPage extends React.Component {
   constructor(props) {
@@ -17,21 +19,22 @@ class ProductIndexPage extends React.Component {
     this.col_hash = this.props.collections.reduce(function ( total, current ) {
         total[ current.title ] = current.id;
         return total;
-      }, {});
+    }, {});
   }
 
   handleTermChange(event) {
-    this.setState({term: event}, () => {
+    this.setState({
+      term: event,
+      collection: null
+    }, () => {
       this.searchProducts()
     });
   }
   handleCollectionChange(event) {
-    console.log('Handle Collection')
-    console.log(this.col_hash['necklace'])
-    console.log(event)
     this.setState({
       collection_id: this.col_hash[event],
-      collection: event
+      collection: event,
+      term: ''
     }, () => {
       this.searchProducts()
     });
@@ -53,37 +56,36 @@ class ProductIndexPage extends React.Component {
     function CollectionTitles(collection) {
       return collection.title
     }
-    // function CollectionHash(){
-    //   var obj = this.props.collections.reduce(function ( total, current ) {
-    //     total[ current.title ] = current.id;
-    //     return total;
-    //   }, {});
-    //   return obj;
-    // }
     return (
       <Layout>
         <Layout.Section>
-          <Card title="Online store dashboard" actions={[{content: 'Edit'}]} sectioned>
+        <Card>
+          <Card.Section>
+            <PriceTestForm />
+          </Card.Section>
+        </Card>
+          <Card title="Online store dashboard" actions={[{content: 'Edit'}]} class="product_list" sectioned>
             <Card.Section>
-              <Stack>
-                <TextField 
-                  value={this.state.term}
-                  label="Keyword Search"
-                  placeholder="Enter Term"
-                  onChange={this.handleTermChange}
-                />
-                <Select
-                  value= {this.state.collection}
-                  label="Collection"
-                  options={ this.props.collections.map(CollectionTitles) }
-                  placeholder="Select"
-                  onChange={this.handleCollectionChange}
-                  //onChange={this.valueUpdater('collection')}
-                  //onChange={this.searchProducts('collection')}
-                />
-              </Stack>
+              <FormLayout>
+                <FormLayout.Group>
+                  <TextField 
+                    value={this.state.term}
+                    label="Keyword Search"
+                    placeholder="Enter Term"
+                    onChange={this.handleTermChange}
+                  />
+                  <Select
+                    value= {this.state.collection}
+                    label="Collection"
+                    options={ this.props.collections.map(CollectionTitles) }
+                    placeholder="Select"
+                    onChange={this.handleCollectionChange}
+                  />
+                </FormLayout.Group>
+              </FormLayout>
             </Card.Section>
             <Card.Section>
+             <Stack vertical='true' alignment='center' distribution='fillEvenly' spacing="loose">
                 <ResourceList
                   items={
                       this.state.products.map(CreateItem)
@@ -92,46 +94,36 @@ class ProductIndexPage extends React.Component {
                     return <ResourceList.Item key={index} {...item} className='Polaris-Card' />;
                   }}
                 />
-            </Card.Section>
+              <Pagination
+                class='pages'
+                hasPrevious
+                onPrevious={() => {console.log('Previous')}}
+                hasNext
+                onNext={() => {console.log('Next')}} 
+              />
+            </Stack>
+          </Card.Section>
           </Card>
+        </Layout.Section>
+        <Layout.Section>
+          <FooterHelp>
+             The Lannister Group © 2017
+          </FooterHelp>
         </Layout.Section>
       </Layout>
     );  
   }
 searchProducts() {
-  console.log('/products/' + "?term=" + this.state.term + "&collection=" + this.state.collection_id)
   $.ajax( {
     url: '/products/' + "?term=" + this.state.term + "&collection=" + this.state.collection_id,
     dataType: 'json',
     success: function(data) {
-    this.setState({ products: data });
+      this.setState({ products: data });
     }.bind(this),
     error: function(data) {
     }.bind(this)
    });
-}
-  
-
-
-  // searchProducts(field) {
-  //   console.log('In queryProducts');
-  //   console.log(this);
-  //   console.log(this.state.value)
-    
-  //   return(value) => {
-  //     if (value){
-  //       $.ajax( {
-  //         url: this.props.products_path + "?term=" + this.state.term, //+ "&collection=" + this.state.collection,
-  //   	    dataType: 'json',
-  //   	    success: function(data) {
-  //         this.setState({ products: data });
-  //   	    }.bind(this),
-  //   	    error: function(data) {
-  //   	    }.bind(this)
-  //   	   });
-  //     }
-  //   }
-  // }
+  }
 }
   
 document.addEventListener('DOMContentLoaded', () => {
