e6568de7c3e63c85de7a095b74f008bf180b30a3
Alex <ageorge010@vt.edu>
2017-09-14 04:52:15 +0000

    AJAX submit form works

--- app/javascript/packs/PriceTestForm.js
+++ app/javascript/packs/PriceTestForm.js
@@ -15,6 +15,7 @@ export default class PriceTestForm extends React.Component {
     };
     this.handlePercentIncreaseChange = this.handlePercentIncreaseChange.bind(this)
     this.handlePercentDecreaseChange = this.handlePercentDecreaseChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   handlePercentIncreaseChange(event) {
     this.setState({percent_increase: event}) 
@@ -22,6 +23,11 @@ export default class PriceTestForm extends React.Component {
   handlePercentDecreaseChange(event) {
     this.setState({percent_decrease: event}) 
   }
+  handleSubmit(event) {
+    console.log('submit')
+    this.createPriceTest()
+  }
+
     render () {
         return (<Card>
                   <FormLayout>
@@ -55,24 +61,26 @@ export default class PriceTestForm extends React.Component {
                         //onChange={this.handleCollectionChange}
                       />
                     </FormLayout.Group>
-                    <Button primary>Start Price Test</Button>
+                    <Button primary onClick={this.handleSubmit}>Start Price Test</Button>
                  </FormLayout>
                </Card>);
     }
   createPriceTest() {
     $.ajax( {
       type: "POST",
+      dataType: "json",
       url: '/price_tests',
-      data: { product_id: this.state.product.id, 
+      data: { price_test: { product_id: this.state.product.id, 
               shopify_product_id: this.state.product.shopify_id, 
               percent_increase: this.state.percent_increase, 
               ending_digits: "0.99", 
-              price_points: 1 },
+              price_points: 1 } },
       success: function(data) {
         console.log('success')
         //this.setState({ products: data });
       }.bind(this),
       error: function(data) {
+        console.log('failed')
       }.bind(this)
      });
   }
