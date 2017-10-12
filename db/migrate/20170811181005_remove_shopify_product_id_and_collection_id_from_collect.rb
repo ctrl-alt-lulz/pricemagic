c045e24f4f880080e3d13995d89611ccf828b100
Alex <ageorge010@vt.edu>
2017-10-12 16:45:52 +0000

    Updates migration to deal with error

--- db/migrate/20170811181005_remove_shopify_product_id_and_collection_id_from_collect.rb
+++ db/migrate/20170811181005_remove_shopify_product_id_and_collection_id_from_collect.rb
@@ -2,7 +2,7 @@ class RemoveShopifyProductIdAndCollectionIdFromCollect < ActiveRecord::Migration
   def change
     add_column :collects, :collection_id, :integer
     add_column :collects, :product_id, :integer
-    remove_column :collects, :shopify_product_id
+    # remove_column :collects, :shopify_product_id
     remove_column :collects, :shopify_collection_id
     rename_column :collects, :collect_id, :shopify_collect_id
   end
