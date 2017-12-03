955641be3285c79e6240aaf88dd1bc2e5aaa0a5f
Alexander George <ageorge010@vt.edu>
2017-12-02 20:01:10 -0800

    fixed worker bugs on bulk destroy, uses product to lookup shop now instead of potentially non-existant price_tests

--- app/workers/bulk_destroy_price_tests_worker.rb
+++ app/workers/bulk_destroy_price_tests_worker.rb
@@ -1,9 +1,9 @@
 class BulkDestroyPriceTestsWorker
   include Sidekiq::Worker
-  sidekiq_options :retry => 1
+  sidekiq_options :retry => 10
   
   def perform(product_ids)
-    shop = PriceTest.find_by(product_id: product_ids.first).shop
+    shop = Product.find(product_ids.first).shop
     shop.with_shopify!
     PriceTest.where(product_id: product_ids, active: true).try(:destroy_all)
   end
