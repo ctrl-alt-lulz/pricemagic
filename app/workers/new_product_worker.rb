fe9823f498f1adbc0e7a034ed35bcaaa2bd7abc3
Alexander George <ageorge010@vt.edu>
2017-12-10 19:23:30 -0800

    more of the same

--- app/workers/new_product_worker.rb
+++ app/workers/new_product_worker.rb
@@ -3,7 +3,11 @@ class NewProductWorker
   sidekiq_options retry: 15
 
   def perform(shopify_product_id, title, id, product_type, tags, variants)
-    variants = JSON.load(variants)
+    puts variants
+    puts '*' * 50
+    puts variants.first
+    puts '*' * 50
+
     shop = Shop.find(id)
     new_product = shop.products.new(title: title, shopify_product_id: shopify_product_id,
                                     product_type: product_type, tags: tags,
