0a8283dbee5e12901cbc21a02b7cb235466f3fe5
Alexander George <ageorge010@vt.edu>
2017-12-19 11:08:48 +0900

    status update not working as expected, modified hanging worker clean up

--- app/workers/destroy_hanging_recurring_charge_worker.rb
+++ app/workers/destroy_hanging_recurring_charge_worker.rb
@@ -5,15 +5,8 @@ class DestroyHangingRecurringChargeWorker
   def perform(id)
     shop = Shop.find(id)
     shop.with_shopify!
-    if shop.recurring_charges.last['charge_data']['status'] == 'pending'
+    if ShopifyAPI::RecurringApplicationCharge.current.nil?
       shop.recurring_charges.delete_all
-    end
-    unless ShopifyAPI::RecurringApplicationCharge.current.nil?
-      begin
-        ShopifyAPI::RecurringApplicationCharge.current.destroy
-      rescue => e
-        puts e.inspect
-      end
     end
   end
 end
