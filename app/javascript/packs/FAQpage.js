24f4a4f61579d6c1ce5da8d02b9e03bc0c9ef5a7
Alexander George <ageorge010@vt.edu>
2018-01-10 17:08:18 -1000

    more FAQ updates

--- app/javascript/packs/FAQpage.js
+++ app/javascript/packs/FAQpage.js
@@ -23,6 +23,36 @@ export default class FAQpage extends React.Component {
                       </TextContainer>
                     </Layout.AnnotatedSection>
                     <Layout.AnnotatedSection>
+                        <TextContainer>
+                            <Heading>What parameters do I need to create a price test?</Heading>
+                            <p> You must enter the number of views per price point to test and at minimum a percent increase value.</p>
+                        </TextContainer>
+                    </Layout.AnnotatedSection>
+                    <Layout.AnnotatedSection>
+                        <TextContainer>
+                            <Heading>Can you give me an example of how a price test works?</Heading>
+                            <p> Let’s say you have a product selling for $20. Set the upper and lower boundary
+                                to 10% each, set two price points, and set 10,000 views. PriceMagic will change your
+                                product price to $18, and collect data until it reaches 10,000 product views,
+                                it will then move the price to $22, and repeat this process. Once it has finished it
+                                will revert to the original price point of $20. You can view the graphs on the product
+                                page to determine what price was best for your product!</p>
+                        </TextContainer>
+                    </Layout.AnnotatedSection>
+                    <Layout.AnnotatedSection>
+                        <TextContainer>
+                            <Heading>How do I create a bulk price test?</Heading>
+                            <p>First, check the box next to each product you want a test created for on the dashboard page.
+                            Second, enter the price test parameters. Third press "Start Price Test".</p>
+                        </TextContainer>
+                    </Layout.AnnotatedSection>
+                    <Layout.AnnotatedSection>
+                        <TextContainer>
+                            <Heading>How do I preview the price test before running it?</Heading>
+                            <p>Click on the product title on the dashboard page and enter the price test parameters on that page.</p>
+                        </TextContainer>
+                    </Layout.AnnotatedSection>
+                    <Layout.AnnotatedSection>
                       <TextContainer>
                         <Heading>How do I view the results of each test?</Heading>
                         <p>Click on the product title on the dashboard page to view the detailed product performance.</p>
